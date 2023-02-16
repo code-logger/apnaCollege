e123190e3aac446d1cae677497e7c54651c4ad91
yogesh <nitesh6pandey6@gmail.com>
2023-02-17 01:47:40 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import logging 
+from helper.InternalApis import course_details
+from helper.misc import extract_course
+
+logging.basicConfig(level=logging.DEBUG,handlers=[
+        logging.FileHandler("app.log"),
+        logging.StreamHandler()
+    ],format='%(levelname)s - %(message)s')
+
+def main(): 
+    logging.debug("Started Running")
+    base_data = course_details()
+    extract_course(base_data)
+if __name__ == "__main__":
+    main() 
