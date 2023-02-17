12a32127784b0a3f62dad96f44c41dc2e8cbf0d3
code-logger <90549995+code-logger@users.noreply.github.com>
2023-02-17 14:34:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 FOLDER_PATH="/home/azar/course/data" # Folder where the course will be saved 
-
 TOKEN=""# TOKEN that was extracted  , for details in Readme File 
 
 COURSE_ID="62d291f4c6792dabb401dcfd"
